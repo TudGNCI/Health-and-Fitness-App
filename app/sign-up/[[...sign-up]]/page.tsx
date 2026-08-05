5649a8be2c0e8e0b692be554d640b1600b3016fa
306034062+TudGNCI@users.noreply.github.com <306034062+TudGNCI@users.noreply.github.com>
2026-08-05 20:06:31 +0100

    minor changes

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,17 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignIn />
+    return (
+      <div className="flex h-[66.66vh] items-center justify-center pt-30">
+        <SignUp 
+          appearance={{
+              variables: {
+                colorPrimary: '#BCBEF5',
+                colorBackground: '#A0A2E8',
+              }
+            }
+          }
+        />
+      </div>
+    );
 }
