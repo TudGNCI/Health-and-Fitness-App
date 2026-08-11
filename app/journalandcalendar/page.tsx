4990603b35306774fd6578bf283e386a79772cbc
306034062+TudGNCI@users.noreply.github.com <306034062+TudGNCI@users.noreply.github.com>
2026-08-11 17:22:15 +0100

    journal

--- app/journalandcalendar/page.tsx
+++ app/journalandcalendar/page.tsx
@@ -1,11 +1,24 @@
 'use client'
 import Link from "next/link"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DayPicker } from "@daypicker/react";
 import "@daypicker/react/style.css";
+import { format } from 'date-fns';
 
 export default function Page() {
-    const [selected, setSelected] = useState<Date | undefined>();
+    const [selectedDay, setSelectedDay] = useState<Date>();
+    const [journalEntry, setJournalEntry] = useState<string>('');
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setJournalEntry (e.target.value)
+
+    const footer = selectedDay ? (
+    <p>You selected {format(selectedDay, 'PPP')}.</p>
+  ) : (
+    <p>Please pick a day.</p>
+  );
+
+    
+
 
   return (
     <main>
@@ -13,8 +26,18 @@ export default function Page() {
         <h1>Journal and Calendar</h1>
         </div>
       <div className="h-[50vh] pt-[10vh] flex items-center justify-center">
-        <DayPicker className="scale-140 bg-white p-4 shadow-lg rounded-lg shadow-zinc-500" mode="single" selected={selected} onSelect={setSelected} 
+        <DayPicker className="scale-140 bg-white p-4 shadow-lg rounded-lg shadow-zinc-500" mode="single" selected={selectedDay} onSelect={setSelectedDay} 
+        footer={footer}
          />
+      </div>
+      <div className="pt-[20vh] pb-[6vh] flex justify-center items-center">
+        <form>
+          <textarea className="placeholder:text-black w-100 min-h-[50px] w-[500px] bg-white"
+          value={journalEntry}
+          onChange={handleChange}
+          placeholder="Write your thoughts...">
+          </textarea>
+        </form>
       </div>
         <Link href='/'
         className="fixed right-4 bottom-4 bg-cyan-600 hover:bg-cyan-700 px-7 py-4 rounded-lg shadow-lg shadow-zinc-500"> BACK
