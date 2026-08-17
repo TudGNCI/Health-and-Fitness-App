c7cabc890176b8b0dbae2c421485e6535cd8c21e
306034062+TudGNCI@users.noreply.github.com <306034062+TudGNCI@users.noreply.github.com>
2026-08-17 23:23:07 +0100

    issue fix

--- prisma/migrations/20260817222236_init/migration.sql
+++ prisma/migrations/20260817222236_init/migration.sql
@@ -8,5 +8,19 @@ CREATE TABLE "JournalEntry" (
     "updateAt" DATETIME NOT NULL
 );
 
+-- CreateTable
+CREATE TABLE "Measurement" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "clerkId" TEXT NOT NULL,
+    "chest" REAL NOT NULL,
+    "waist" REAL NOT NULL,
+    "arms" REAL NOT NULL,
+    "hips" REAL NOT NULL,
+    "date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "JournalEntry_userId_date_key" ON "JournalEntry"("userId", "date");
+
+-- CreateIndex
+CREATE INDEX "Measurement_clerkId_idx" ON "Measurement"("clerkId");
